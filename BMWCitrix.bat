89145e42d8fb14cdbf38f7efa85a20c287c22887
sagar2503 <>
2018-09-22 01:56:11 +0530

    modified bat file

--- BMWCitrix.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\a647245\AppData\Local\UiPath\app-18.2.3
-
-UiRobot.exe -file C:\PrivateRepo\RPA\CitrixAutomation\BMWCitrixAutomatedLogin_IE.xaml
---
+++ CitrixAutomation/BMWCitrix.bat
@@ -0,0 +1,1 @@
+cd C:\Users\a647245\AppData\Local\UiPath\app-18.2.3\UiRobot.exe -file C:\PrivateRepo\RPA\CitrixAutomation\BMWCitrixAutomatedLogin_IE.xaml
